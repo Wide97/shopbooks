206618a5e06f53e14595aea8098e9583bd0ff3bb
Wide97 <mwidesott@gmail.com>
2024-08-21 16:30:09 +0200

    m2 d3

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import MyNav from './Component/MyNav';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Welcome from './Component/Welcome';
 import SingleBook from './Component/SingleBook';
+import books from './Books/fantasy.json';
 // import AllTheBooks from './Component/AllTheBooks';
 
 function App() {
@@ -16,7 +17,7 @@ function App() {
       <main>
       <Welcome/>
       {/* <AllTheBooks/> */}
-      <SingleBook/>
+      <SingleBook book={books[0]}/>
     </main>
     <footer>
       <MyFooter/>
--- src/Component/SingleBook.jsx
+++ src/Component/SingleBook.jsx
@@ -1,38 +1,25 @@
 import { Component } from "react";
-import { Row, Col, Card, Button } from "react-bootstrap";
-import fantasy from '../Books/fantasy.json'
+import { Card, Button } from "react-bootstrap";
+
+
 
 class SingleBook extends Component {
-  render() {
-    return (
-      <Row className="justify-content-center">
-        
-          <Col
-            key={this.props.libro.asin}
-            xs={12}
-            md={4}
-            className="p-4 d-flex justify-content-center"
-          >
-            <Card style={{ width: "75%" }}>
-              <Card.Img
-                className="d-flex flex-column h-100"
-                variant="top"
-                src={this.props.libro.img}
-                alt="book img"
-              />
-              <Card.Body>
-                <Card.Title className="text-center pt-4 text-danger">
-                  {this.props.libro.title}
-                </Card.Title>
-                <div className="mt-4 d-flex justify-content-center">
-                  <Button variant="primary">{`$${this.props.libro.price}`}</Button>
-                </div>
-              </Card.Body>
+    render() {
+        const { book } = this.props; 
+
+        return (
+            <Card style={{ width: '15%' }}>
+                <Card.Img variant="top" src={book.img} alt={book.title} />
+                <Card.Body>
+                    <Card.Title>{book.title}</Card.Title>
+                    <Button variant="primary">{book.price}</Button>
+                </Card.Body>
             </Card>
-          </Col>
-      </Row>
-    );
-  }
+        );
+    }
 }
 
 export default SingleBook;
+  
+
+
