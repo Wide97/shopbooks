18d8edb5f47dba4d7e05b17b916d83566b5beb4f
Wide97 <mwidesott@gmail.com>
2024-08-26 16:05:43 +0200

    m2 d1

--- src/Component/CommentArea.jsx
+++ src/Component/CommentArea.jsx
@@ -1,67 +1,44 @@
-import React, { Component } from 'react';
-import { Spinner, Alert } from 'react-bootstrap';
-import CommentsList from './CommentsList';
-import AddComment from './AddComment';
+import React, { Component } from "react";
+import { Spinner, Alert } from "react-bootstrap";
+import CommentsList from "./CommentsList";
+import AddComment from "./AddComment";
 
 class CommentArea extends Component {
   state = {
     comments: [],
-    isLoading: true,
+    isLoading: false,
     error: null,
   };
 
   componentDidUpdate(prevProps) {
     if (this.props.bookId && this.props.bookId !== prevProps.bookId) {
-      this.fetchComments(); 
+      this.fetchComments();
     }
   }
 
   fetchComments = () => {
-    fetch(`https://striveschool-api.herokuapp.com/api/comments/${this.props.bookId}`, {
-      headers: {
-        "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NmEzNjZjOWYyNjBjYzAwMTVjYzBkZmYiLCJpYXQiOjE3MjQzMzI3NzgsImV4cCI6MTcyNTU0MjM3OH0.Nem5TBCFtAHGg6RQPOw2lmZjcCCB_W2P1_lJMf-4ZX0"
+    this.setState({ isLoading: true, error: null });
+
+    fetch(
+      `https://striveschool-api.herokuapp.com/api/comments/${this.props.bookId}`,
+      {
+        headers: {
+          Authorization:
+            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NmEzNjZjOWYyNjBjYzAwMTVjYzBkZmYiLCJpYXQiOjE3MjQzMzI3NzgsImV4cCI6MTcyNTU0MjM3OH0.Nem5TBCFtAHGg6RQPOw2lmZjcCCB_W2P1_lJMf-4ZX0",
+        },
       }
-    })
-      .then(response => {
+    )
+      .then((response) => {
         if (!response.ok) {
-          throw new Error('Network response was not ok');
+          throw new Error("Network response was not ok");
         }
         return response.json();
       })
-      .then(data => {
+      .then((data) => {
         this.setState({ comments: data, isLoading: false });
       })
-      .catch(error => {
+      .catch((error) => {
         this.setState({ error: error.message, isLoading: false });
-      });
-  };
-
-  handleAddComment = (commentText, rating) => {
-    const commentData = {
-      comment: commentText,
-      rate: rating,
-      elementId: this.props.bookId
-    };
-
-    fetch('https://striveschool-api.herokuapp.com/api/comments/', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NmEzNjZjOWYyNjBjYzAwMTVjYzBkZmYiLCJpYXQiOjE3MjQzMzI3NzgsImV4cCI6MTcyNTU0MjM3OH0.Nem5TBCFtAHGg6RQPOw2lmZjcCCB_W2P1_lJMf-4ZX0"
-      },
-      body: JSON.stringify(commentData)
-    })
-      .then(response => {
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        return response.json();
-      })
-      .then(() => {
-        this.fetchComments(); 
-      })
-      .catch(error => {
-        this.setState({ error: error.message });
       });
   };
 
@@ -71,12 +48,17 @@ class CommentArea extends Component {
     return (
       <div>
         {isLoading && <Spinner animation="border" />}
-        {error && <Alert variant="danger">C'è stato un errore: {error}</Alert>}
-        {!isLoading && !error && (
+        {error && (
+          <Alert variant="danger">C'è stato un errore: {error}</Alert>
+        )}
+        {!isLoading && !error && comments.length > 0 && (
           <div>
             <CommentsList comments={comments} />
             <AddComment onAddComment={this.handleAddComment} />
           </div>
+        )}
+        {!isLoading && !error && comments.length === 0 && (
+          <p className="text-center">Nessun commento disponibile per questo libro.</p>
         )}
       </div>
     );
