186882d5d4523922c87432ad14aef1a3fb2b7e87
Wide97 <mwidesott@gmail.com>
2024-08-27 16:23:02 +0200

    m3 d3

--- src/components/SingleBook.jsx
+++ src/components/SingleBook.jsx
@@ -1,36 +1,35 @@
-import { Component } from 'react'
+import React from 'react'
 import { Card } from 'react-bootstrap'
 // import CommentArea from './CommentArea'
 
-class SingleBook extends Component {
+const SingleBook = ({ book, selectedBook, changeSelectedBook }) => {
   // state = {
   //   selected: false,
   // }
 
-  render() {
     return (
       <>
         <Card
           // onClick={() => this.setState({ selected: !this.state.selected })}
-          onClick={() => this.props.changeSelectedBook(this.props.book.asin)}
+          onClick={() => changeSelectedBook(book.asin)}
           style={{
             border:
-              this.props.selectedBook === this.props.book.asin
+              selectedBook === book.asin
                 ? '3px solid red'
                 : 'none',
           }}
         >
-          <Card.Img variant="top" src={this.props.book.img} />
+          <Card.Img variant="top" src={book.img} />
           <Card.Body>
             <Card.Title style={{ color: 'black' }}>
-              {this.props.book.title}
+              {book.title}
             </Card.Title>
           </Card.Body>
         </Card>
-        {/* {this.state.selected && <CommentArea asin={this.props.book.asin} />} */}
+        {/* {this.state.selected && <CommentArea asin={book.asin} />} */}
       </>
     )
   }
-}
+
 
 export default SingleBook
