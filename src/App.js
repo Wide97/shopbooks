bb0aabd005efdd3b67b92769c7711c9288daa7dd
Wide97 <mwidesott@gmail.com>
2024-08-27 14:32:50 +0200

    m3 d2

--- src/App.js
+++ src/App.js
@@ -1,53 +1,26 @@
-import "./App.css";
-import MyFooter from "./Component/MyFooter";
-import MyNav from "./Component/MyNav";
-import "bootstrap/dist/css/bootstrap.min.css";
-import Welcome from "./Component/Welcome";
-import books from "./Books/fantasy.json";
-import BookList from "./Component/BookList";
-import { Col, Row } from "react-bootstrap";
-import CommentArea from "./Component/CommentArea";
-import { Component } from "react";
-// import AllTheBooks from './Component/AllTheBooks';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css'
+import MyNav from './components/MyNav'
+import MyFooter from './components/MyFooter'
+import Welcome from './components/Welcome'
+// import AllTheBooks from './components/AllTheBooks'
+import { Container } from 'react-bootstrap'
+import BookList from './components/BookList'
 
-class App extends Component {
-  state = {
-    selectedBookAsin: null,
-  };
+import fantasy from './data/fantasy.json'
 
-  changeSelectedBook = (asin) => {
-    this.setState({
-      selectedBookAsin: asin,
-    });
-  };
-
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <MyNav />
-        </header>
-        <main>
-          <Welcome />
-          <Row>
-            <Col xs={12} md={8} lg={6}>
-              {/* <AllTheBooks/> */}
-              <BookList
-                books={books}
-                onBookSelected={this.changeSelectedBook}
-              />
-            </Col>
-            <Col xs={12} md={4}>
-              <CommentArea bookId={this.state.selectedBookAsin} />
-            </Col>
-          </Row>
-        </main>
-        <footer>
-          <MyFooter />
-        </footer>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <>
+      <MyNav />
+      <Container>
+        <Welcome />
+        {/* <AllTheBooks /> */}
+        <BookList books={fantasy} />
+      </Container>
+      <MyFooter />
+    </>
+  )
 }
 
-export default App;
+export default App
